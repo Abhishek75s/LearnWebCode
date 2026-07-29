580e351b84e9c949b91baae4be1c058cae8a1a42
Abhishek Srivastava <abhi.ss9636@gmail.com>
2026-07-29 23:53:56 +0530

    added ejs views and their respecivce pages to render on url hit

--- server.js
+++ server.js
@@ -2,8 +2,27 @@ import express from 'express';
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-    res.send('All good!!')
+    res.render('homepage.ejs')
+});
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs');
 });
 
 app.listen(3000);
+console.log('Server running at PORT: 3000');
+
+
+/*
+to seee the live preview within the VS code editor itself:
+
+    -> go to ctrl + shift + P
+    -> go to browser: 
+        -> Open Intergrated Browser in VS code
+        -> Enter the url like: http://localhost:3000
+
+*/
